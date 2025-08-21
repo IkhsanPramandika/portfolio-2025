186e6e6c3fca6ffbc0ab4761002ffe55c30c6ab9
MUHAMMAD IKHSAN PRAMANDIKA <mikhsanpra26@gmail.com>
2025-08-21 07:25:00 +0700

    update cv

--- modules/about/components/Resume.tsx
+++ modules/about/components/Resume.tsx
@@ -5,7 +5,7 @@ import { LuDownload as DownloadIcon } from "react-icons/lu";
 const Resume = () => {
   const t = useTranslations("AboutPage");
 
-  const RESUME_URL = "https://bit.ly/cv-satriabahari";
+  const RESUME_URL = "https://drive.google.com/file/d/1GjcBCOHR0RdFugE7qE2XiJEoutlK31fm/view?usp=sharing";
 
   return (
     <Link
